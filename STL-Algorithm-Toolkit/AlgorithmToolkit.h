8281d172480ec3b00b67cad0056700c689343491
chiliu5467 <chiliu5467@gmail.com>
2026-09-15 13:50:01 +0800

    feat: add generic iterator-based FindValue

--- STL-Algorithm-Toolkit/AlgorithmToolkit.h
+++ STL-Algorithm-Toolkit/AlgorithmToolkit.h
@@ -4,8 +4,22 @@
 template <typename Iterator>
 void PrintRange(Iterator first, Iterator last)
 {
-    for (auto it = first; it != last; it++)
+    for (auto it = first; it != last; ++it)
     {
 		std::cout << *it << "\n";
     }
+}
+
+template <typename Iterator, typename T>
+Iterator FindValue(Iterator first, Iterator last, const T& value)
+{
+    for (auto it = first; it != last; ++it)
+    {
+        if (*it == value)
+        {
+            return it;
+        }
+    }
+
+    return last;
 }
--- STL-Algorithm-Toolkit/main.cpp
+++ STL-Algorithm-Toolkit/main.cpp
@@ -14,14 +14,54 @@ void TestPrintRange()
 	std::list<int> numbers2{ 10, 20, 30, 40 };
 	PrintRange(numbers2.begin(), numbers2.end());
 
-	std::vector<std::string> string{ "apple", "banana", "cat", "dog" };
-	PrintRange(string.begin(), string.end());
+	std::vector<std::string> words{ "apple", "banana", "cat", "dog" };
+	PrintRange(words.begin(), words.end());
 
 	std::cout << "------------------------------" << std::endl;
 }
+
+void TestFindValue()
+{
+	std::cout << "=== Test Find Value ===" << std::endl;
+	std::vector<int> numbers{ 10, 20, 30, 40 };
+	auto it = FindValue(numbers.begin(), numbers.end(), 30);
+	if (it != numbers.end())
+	{
+		std::cout << "Found value: " << *it << std::endl;
+	}
+	else
+	{
+		std::cout << "Value not found." << std::endl;
+	}
+
+	std::list<int> listNumbers{ 10, 20, 30, 40 };
+	auto it1 = FindValue(listNumbers.begin(), listNumbers.end(), 50);
+	if (it1 != listNumbers.end())
+	{
+		std::cout << "Found value: " << *it1 << std::endl;
+	}
+	else
+	{
+		std::cout << "Value not found." << std::endl;
+	}
+
+	std::list<std::string> words{ "apple", "banana", "cat", "dog" };
+	auto it2 = FindValue(words.begin(), words.end(), std::string("cat"));
+	if (it2 != words.end())
+	{
+		std::cout << "Found value: " << *it2 << std::endl;
+	}
+	else
+	{
+		std::cout << "Value not found." << std::endl;
+	}
+	std::cout << "------------------------------" << std::endl;
+}
+
 int main()
 {
 	TestPrintRange();
+	TestFindValue();
 
 	return 0;
 }
